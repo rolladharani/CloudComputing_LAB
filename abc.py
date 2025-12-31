dcc07602e8cadee62dec2422a8f414dbb46c1603
Dharani R <144825716+rolladharani@users.noreply.github.com>
2025-12-31 15:23:49 +0530

    Update1 abc.py

--- abc.py
+++ abc.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/hello", methods = ['GET'])
 def hello():
-  return "Hello UmaPavan"
+  return "Dharani Sri Maha"
 if __name__ == "__main__:
   app.run(debug=True)
